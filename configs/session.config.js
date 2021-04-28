1bde61bd72a2fc1d47f6911016a67f60cf6f3d62
veroca88 <veronicadonoso1506@gmail.com>
2021-04-27 19:22:21 -0500

    Increase time in cookie

--- configs/session.config.js
+++ configs/session.config.js
@@ -6,9 +6,11 @@ module.exports = app => {
     app.use(
         session({
             secret: process.env.SESS_SECRET,
-            cookie: { maxAge: 60000 },
+            cookie: { maxAge: 3600000 },
             resave: true,
             saveUninitialized: true,
+            duration: 24 * 60 * 60 * 1000,
+            acrtiveDution: 1000 * 60 * 5,
             store: new MongoStore({
                 mongooseConnection: mongoose.connection,
                 ttl: 60 * 60 * 24 // 1 day
